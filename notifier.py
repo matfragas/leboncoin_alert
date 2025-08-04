4550120b95d272a2afc83c0e0c6f48e1b5f354cd
matfragas <franck.gasseau@gmail.com>
2025-08-04 11:25:09 +0200

    Create notifier.py

---
+++ notifier.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.mime.text import MIMEText
+
+EMAIL_SENDER = "ton@email.com"
+EMAIL_PASSWORD = "motdepasse"
+EMAIL_RECEIVER = "destinataire@email.com"
+
+def send_alert(ad, category):
+    msg = MIMEText(f"NOUVELLE ANNONCE [{category}]\n\n{ad['title']}\n{ad['price']}\n{ad['url']}")
+    msg['Subject'] = f"Nouvelle annonce sur LeBonCoin - {category}"
+    msg['From'] = EMAIL_SENDER
+    msg['To'] = EMAIL_RECEIVER
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(EMAIL_SENDER, EMAIL_PASSWORD)
+        server.send_message(msg)
