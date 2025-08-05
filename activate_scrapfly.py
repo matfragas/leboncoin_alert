d52c58450f8c614075ba416a0b9a7f2f43e9d0e2
matfragas <franck.gasseau@gmail.com>
2025-08-05 15:51:13 +0200

    Update activate_scrapfly.py

--- activate_scrapfly.py
+++ activate_scrapfly.py
@@ -1,6 +1,6 @@
 import requests
 
-SCRAPFLY_KEY = os.getenv("SCRAPFLY_API_KEY")  # Remplace par ta clé
+SCRAPFLY_KEY = "scp-live-69f48ed54ff0440398714a7095f09269"  # Remplace par ta clé
 
 url = "https://api.scrapfly.io/scrape"
 params = {
